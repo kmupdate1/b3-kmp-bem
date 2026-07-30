08cbf6314be915af8ea2f656acc98a857dd6afc8
Ken <lax.potcx1562@gmail.com>
2026-07-30 11:47:52 +0900

    依存関係整理

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -11,7 +11,6 @@ kotlin {
         commonMain.dependencies {
             implementation(libs.kotlinx.serialization.json)
 
-            api(project(":core"))
             api(project(":codec"))
         }
         commonTest.dependencies {
