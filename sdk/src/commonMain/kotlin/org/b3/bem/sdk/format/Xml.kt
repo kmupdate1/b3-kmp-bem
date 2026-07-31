daef3b5a37c8b08f39c7192723f8bc286e798383
Ken <lax.potcx1562@gmail.com>
2026-08-01 02:34:48 +0900

    フラクタル構造を活用して解決

--- sdk/src/commonMain/kotlin/org/b3/bem/sdk/format/Xml.kt
+++ sdk/src/commonMain/kotlin/org/b3/bem/sdk/format/Xml.kt
@@ -5,15 +5,12 @@ import org.b3.bem.codec.codec.Codec
 import org.b3.bem.codec.xml.XmlCodec
 
 object Xml : Format<String> {
-    override val codec: Codec<String> = XmlCodec(
-        xml = XML.recommended_1_0 {
-            indentString = "  "
-        },
-        // declaration = false,
-    )
+    override val codec: Codec<String> = XmlCodec(XML.recommended_1_0 {
+        indentString = "  "
+    })
 
     override fun document(parts: List<String>): String =
-        buildString{
+        buildString {
             append("""<?xml version="1.1"?>""")
             append("\n")
             append("<Facts>\n")
