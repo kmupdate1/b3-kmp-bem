97a6b5d6beb9bc3011d328962078722d33ca048c
Ken <lax.potcx1562@gmail.com>
2026-08-02 11:08:54 +0900

    通信前にプロトコル整備

--- dsl/build.gradle.kts
+++ dsl/build.gradle.kts
@@ -3,7 +3,6 @@ import org.gradle.kotlin.dsl.`maven-publish`
 plugins {
     `maven-publish`
     alias(libs.plugins.kotlin.multiplatform)
-    // alias(libs.plugins.kotlin.plugin.serialization)
 }
 
 kotlin {
