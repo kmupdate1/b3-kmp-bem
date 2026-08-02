08e9c61754db11bca5a16eb044a17d3b11db16f2
Ken <lax.potcx1562@gmail.com>
2026-08-02 11:09:05 +0900

    サーバ準備

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,8 +1,15 @@
 plugins {
+    `maven-publish`
     alias(libs.plugins.kotlin.jvm)
 }
 
 dependencies {
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.cio)
+    implementation(libs.ktor.server.call.logging)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.server.status.pages)
+
     api(project(":codec"))
     api(project(":protocol"))
 
